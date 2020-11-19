c49c7db5e36a9fa15795e86197182fd920668a57
hemei <hem@253.com>
2020-11-19 23:01:36 +0800

    完数

---
+++ programapplet/program_applet_16.py
@@ -0,0 +1,24 @@
+'''
+
+【程序16】
+
+题目：一个数如果恰好等于它的因子之和，这个数就称为“完数”。例如6=1＋2＋3.编程
+　　　找出1000以内的所有完数。
+解析：
+    先分解这个数，然后判断相加是否等于这个数；
+'''
+
+
+class PerfectNumber():
+
+    def perfectnumber(self):
+        for i in range(1, 1000):
+            lista = []
+            for j in range(1, i):
+                if i % j == 0:
+                    lista.append(j)
+            if i == sum(lista):
+                print(f"{i}={'+'.join(map(str, lista))}")
+
+if __name__ == "__main__":
+    PerfectNumber().perfectnumber()
